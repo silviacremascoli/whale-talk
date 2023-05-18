91b63f2e716f1e55fddf3d4bd56315ad91d02274
silviacremascoli <silcremascoli@gmail.com>
2023-05-18 16:19:55 +0200

    Added second loop and if statement

--- main.js
+++ main.js
@@ -5,5 +5,11 @@ const vowels = ["a", "e", "i", "o", "u"];
 let resultArray = [];
 
 for (let i = 0; i < input.length; i++) {
-  console.log(`i is ${i}`);
+  // console.log(`i is ${i}`);
+  for (let j = 0; j < vowels.length; j++) {
+    // console.log(`j is ${j}`);
+    if (input[i] === vowels[j]) {
+      resultArray.push(vowels[j]);
+    }
+  }
 }
